a7bf373dc561dde52aa563d753bdac8f6babb6a3
DerekRdgz <derek_rdgz@outlook.com>
2025-09-06 19:11:56 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+import os
+from config.db import init_db, mysql
+from dotenv import load_dotenv
+from routes.tareas import tareas_bp
+
+#Importamos la ruta del blueprint
+from routes.tareas import tareas_bp
+from routes.usuarios import usuarios_bp
+
+# Cargar variables de entorno
+load_dotenv()
+
+def create_app(): #<- Creando la funcion
+
+    # Instancia de la app
+    app = Flask(__name__)
+
+    # Configurar la base de datos
+    init_db(app)
+
+    # Registrar el blueprint
+    app.register_blueprint(tareas_bp, url_prefix='/tareas')
+    app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
+
+    return app
+
+#Crear la app
+app = create_app()
+
+if __name__ == '__main__':
+
+    #Obtenemos el puerto de las variables de entorno
+    port = os.getenv("PORT", 8080)
+
+    app.run(host="0.0.0.0", port=port, debug = True)
